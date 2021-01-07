2032769f45cc4ba342f84bdb59d0091dff3f3f61
raghavt20 <raghavtandon2000@gmail.com>
2021-01-07 15:25:42 +0530

    violet: Update fingerprint to Pixel 5 (RQ1A.210105.003)

--- wave_violet.mk
+++ wave_violet.mk
@@ -34,4 +34,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet"
 
+# Fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
