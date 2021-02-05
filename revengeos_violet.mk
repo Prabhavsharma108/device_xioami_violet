c23a51904beb176d66b91a83be7f4f33e1d61ba7
prabhavsharma108 <prabhavsharma108@gmail.com>
2021-02-05 06:19:48 +0000

    Changed fp to feb

--- revengeos_violet.mk
+++ revengeos_violet.mk
@@ -1,39 +1,42 @@
+# Copyright (C) 2020-2021 Wave-OS
 #
-# Copyright (C) 2020 The LineageOS Project
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier: Apache-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-
-# Inherit some common RevengeOS stuff
-TARGET_GAPPS_ARCH := arm64
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/revengeos/config/common.mk)
-
-# Official-ify
-REVENGEOS_BUILDTYPE := OFFICIAL
-
-# Inherit from Violet device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := Violet
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := revengeos_violet
-PRODUCT_MODEL := Redmi Note 7 pro
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Inherit some common WaveOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+REVENGEOS_BUILDTYPE := OFFICIAL
+
+
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+PRODUCT_NAME := revengeos_violet
+PRODUCT_DEVICE := violet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7 Pro
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys" \
-    PRODUCT_NAME="RMX1921"
+    PRODUCT_NAME="violet"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+# Fingerprint
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
